8c5df5ac3df0167ecf183ff058dbfaf580995fe1
Flavio Marques <Flaviomarques@me.com>
2026-06-14 20:02:39 -0300

    auto-save: 20:02:39

--- ios/p1fast-ios/Sources/Views/ContentView.swift
+++ ios/p1fast-ios/Sources/Views/ContentView.swift
@@ -194,6 +194,7 @@ private struct ReadyRoot: View {
     @StateObject private var voltaVideoRepo: VoltaVideoRepository
     @StateObject private var manutencaoStore: ManutencaoConsumiveisStore
     @StateObject private var pecaRepo: PecaRepository
+    @StateObject private var estoqueRepo: EstoqueRepository
     /// Histórico de navegação estável (ver NavRouter em HomeView.swift):
     /// criado UMA vez aqui pra não se perder quando os repositórios acima
     /// publicam e re-renderizam esta view.
