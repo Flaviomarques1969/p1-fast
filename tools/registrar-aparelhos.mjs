fd1daf51ed3443620dfce3c41376b2a040313c51
Flavio Marques <Flaviomarques@me.com>
2026-06-17 11:42:30 -0300

    auto-save: 11:42:30

--- tools/registrar-aparelhos.mjs
+++ tools/registrar-aparelhos.mjs
@@ -14,6 +14,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 import { appendFileSync } from 'fs';
+import { avaliar as avaliarChecklist, imprimir as imprimirChecklist } from './checklist-aparelhos.mjs';
 
 const SUPABASE_URL  = 'https://fvhwltzhytpnhlqbttmd.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2aHdsdHpoeXRwbmhscWJ0dG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTExNDAsImV4cCI6MjA5MzM4NzE0MH0._ZpxksUnuVFhLzCB5x7bBiZ_VLQQR5cH4A1T-0-mvrA';
